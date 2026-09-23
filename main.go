49f5319be9b81e69ed8da802660c98c0f6a38f73
asimanye <asimanye@wethinkcode.co.za>
2026-09-23 20:49:09 +0200

    Copy and paste from the article

--- main.go
+++ main.go
@@ -4,42 +4,64 @@ import (
     "fmt"
     "github.com/gorilla/websocket"
     "net/http"
+    "sync"
 )
 
-// Upgrader is used to upgrade HTTP connections to WebSocket connections.
 var upgrader = websocket.Upgrader{
     CheckOrigin: func(r *http.Request) bool {
        return true
     },
 }
 
+var clients = make(map[*websocket.Conn]bool) // Connected clients
+var broadcast = make(chan []byte)            // Broadcast channel
+var mutex = &sync.Mutex{}                    // Protect clients map
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-    // Upgrade the HTTP connection to a WebSocket connection
     conn, err := upgrader.Upgrade(w, r, nil)
     if err != nil {
        fmt.Println("Error upgrading:", err)
        return
     }
     defer conn.Close()
-    // Listen for incoming messages
+
+    mutex.Lock()
+    clients[conn] = true
+    mutex.Unlock()
+
     for {
-       // Read message from the client
        _, message, err := conn.ReadMessage()
        if err != nil {
-          fmt.Println("Error reading message:", err)
+          mutex.Lock()
+          delete(clients, conn)
+          mutex.Unlock()
           break
        }
-       fmt.Printf("Received: %s\\n", message)
-       // Echo the message back to the client
-       if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
-          fmt.Println("Error writing message:", err)
-          break
+       broadcast <- message
+    }
+}
+
+func handleMessages() {
+    for {
+       // Grab the next message from the broadcast channel
+       message := <-broadcast
+
+       // Send the message to all connected clients
+       mutex.Lock()
+       for client := range clients {
+          err := client.WriteMessage(websocket.TextMessage, message)
+          if err != nil {
+             client.Close()
+             delete(clients, client)
+          }
        }
+       mutex.Unlock()
     }
 }
 
 func main() {
     http.HandleFunc("/ws", wsHandler)
+    go handleMessages()
     fmt.Println("WebSocket server started on :5000")
     err := http.ListenAndServe(":5000", nil)
     if err != nil {
